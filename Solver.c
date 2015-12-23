a2220fb8a916490352e52316b99d6d699637d0bf
Dev <dev.rangarajan@gmail.com>
2015-12-23 03:10:22 -0800

    more gears, variable options, all neverrests

--- Solver.c
+++ Solver.c
@@ -9,45 +9,16 @@ Dev Rangarajan, FTC 5206 The Knights of Ni
 //TODO: Add in part numbers and materials to gear list
 //TODO: Multiple options
 //TODO: aim high/low
-//TODO: Give range (for sizes)
 #define fileName "Gears.txt"
-int gearOptions[25];
+#define gearOptionSize sizeof gearOptions/sizeof *gearOptions
+#define possibleSolutionSize sizeof possibleSolutions/sizeof *possibleSolutions
+
+
+int gearOptions[27];
 int driveGear = 1;
 int drivenGear = 1;
 int motorRatio;
-int numberSolutions; //Uninmplemented. It should find the most compact solution right now.
-
-bool closerTo(float val1, float val2, float target)//True is when val1 is closer
-{
-	if(fabsf(target - val1) < fabsf(target - val2))
-		return 1;
-	else if(fabsf(target - val1) > fabsf(target - val2))
-		return 0;
-	else
-		return 0;
-}
-
-void solve(float desiredRatio, size_t length)
-{
-	float currentRatio = 0;
-	float previousRatio;
-	for(int i = 0; i < length; i++)
-	{
-		if(gearOptions[i] == 0)
-			continue;
-		for(int j = 0; j < length; j++)
-		{
-			if(gearOptions[j] == 0)
-				continue;
-			currentRatio = (float)gearOptions[j]/gearOptions[i] * motorRatio;
-			if(closerTo(currentRatio, (float)drivenGear/driveGear*motorRatio, desiredRatio))
-			{
-				driveGear = gearOptions[i];
-				drivenGear = gearOptions[j];
-			}
-		}
-	}
-}
+int possibleSolutions[20][2];
 
 void populateArray()
 {
@@ -70,6 +41,99 @@ void populateArray()
 	fclose(IO);
 }
 
+bool closerTo(float val1, float val2, float target)//True is when val1 is closer
+{
+	if(fabsf(target - val1) < fabsf(target - val2))
+		return 1;
+	else if(fabsf(target - val1) > fabsf(target - val2))
+		return 0;
+	else
+		return 0;
+}
+
+bool checkDuplicate(int val1, int val2, size_t topx)
+{
+	for(int i = 0; i < topx; i++)
+	{
+		if((possibleSolutions[i][0] == val1) && (possibleSolutions[i][1] == val2))
+			return 0;
+	}
+	return 1;
+}
+
+void solve(float desiredRatio, size_t length)
+{
+	float currentRatio = 0;
+	for(int i = 0; i < length; i++)
+	{
+		if(gearOptions[i] == 0)
+			continue;
+		for(int j = 0; j < length; j++)
+		{
+			if(gearOptions[j] == 0)
+				continue;
+			currentRatio = (float)gearOptions[j]/gearOptions[i] * motorRatio;
+			if(closerTo(currentRatio, (float)drivenGear/driveGear*motorRatio, desiredRatio))
+			{
+				driveGear = gearOptions[i];
+				drivenGear = gearOptions[j];
+			}
+		}
+	}
+}
+
+void solveMany(float desiredRatio, size_t lengthGears, size_t topx)
+{
+	for(int i = 0; i < topx; i++)
+	{
+		possibleSolutions[i][0] = 1;
+		possibleSolutions[i][1] = 1;
+	}
+
+	for(int h = 0; h < topx; h++)
+	{
+		float currentRatio = 0;
+		for(int i = 0; i < lengthGears; i++)
+		{
+			if(gearOptions[i] == 0)
+				continue;
+			for(int j = 0; j < lengthGears; j++)
+			{
+				if(gearOptions[j] == 0)
+					continue;
+				currentRatio = (float)gearOptions[j]/gearOptions[i] * motorRatio;
+				if(!checkDuplicate(gearOptions[j], gearOptions[i], topx))
+				{
+					if(h != 0)
+						continue;
+				}
+				if(closerTo(currentRatio, (float)possibleSolutions[h][0]/possibleSolutions[h][1]*motorRatio, desiredRatio))
+				{
+					possibleSolutions[h][1] = gearOptions[i];
+					possibleSolutions[h][0] = gearOptions[j];
+				}
+			}
+		}
+	}
+}
+
+float getError(int actualDrivenGear, int actualDriveGear, float target)
+{
+	return 100*(float)actualDrivenGear/actualDriveGear*motorRatio/target;
+}
+
+void getForMotor(int motorValue, int numberToPrint, float target)
+{
+	motorRatio = motorValue;
+	solveMany(target, gearOptionSize, possibleSolutionSize);
+	printf("------NeverRest %i------\n", motorRatio);
+	for(int i = 0; i < numberToPrint; i++)
+	{
+		printf("%i:%i, Accuracy: %.2f%%\n", possibleSolutions[i][0], possibleSolutions[i][1],
+ 			getError(possibleSolutions[i][0], possibleSolutions[i][1], 80));
+	}
+}
+
 int main()
 {
 	populateArray();
@@ -77,15 +141,17 @@ int main()
 	printf("Welcome to the gear ratio optimizer. Version 1.0\n\n");
 	while(continueRunning)
 	{
-		printf("Please enter the gearbox ratio on the motor: ");
-		scanf("%i", &motorRatio);
-		printf("Please enter the target ratio (not including motor): ");
+		printf("Please enter the target ratio (including motor): ");
 		float target;
 		scanf("%f", &target);
-		solve(target*motorRatio, sizeof gearOptions/sizeof *gearOptions);
-		printf("These are the closest gears we could find (at the smallest size): \n", motorRatio, target);
-		printf("Drive gear: %i\n", driveGear);
-		printf("Driven gear: %i\n", drivenGear);
+		printf("Please enter the number of options you would like to see for each motor: ");
+		int options;
+		scanf("%i", &options);
+		if(options > possibleSolutionSize)
+			options = possibleSolutionSize;
+		getForMotor(20, options, target);
+		getForMotor(40, options, target);
+		getForMotor(60, options, target);
 		printf("\nWould you like to enter another ratio? (y/n)\n");
 		char answer[5];
 		scanf("%s", &answer);
